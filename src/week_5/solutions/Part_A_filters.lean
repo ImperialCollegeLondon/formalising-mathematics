3c0824633ec64889a592c7dd7b43720e6e4bbed5
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-02-18 12:30:49 +0000

    finished rewrite

--- src/week_5/solutions/Part_A_filters.lean
+++ src/week_5/solutions/Part_A_filters.lean
@@ -1,23 +1,33 @@
 import order.filter.basic -- the basics
-/-
 
+/-
 # Filters
 
 ## Introduction
 
 A topological space structure on a type `α` is a collection of subsets of `α`
-satisfying some axioms. A filter is a similar kind of thing -- a collection
-of subsets of `α` satisfying some different axioms.
+(the open sets) satisfying some axioms. A filter is a similar kind of thing --
+it's a collection of subsets of `α` satisfying some different axioms.
 
-Filters can be used to express a very general idea of a limit: for
-example the limit of a sequence `a n` as `n → ∞`, the limit of a
-continuous function `f(x)` as `x → x₀` or as `x → +∞` or as `x` tends
-to `x₀` from above or... . You could say that they are a beautiful abstraction
-of the concept of a limit. 
+Before we go into the formal definition, let us start with the picture.
+A filter on `α` is an extremely powerful generalisation of the concept
+of a subset of `α`. If `S : set α` is a subset, then there is a principal
+filter `𝓟 S` corresponding to `S` -- it is just the collection of all subsets
+of `α` which contain `S`. However there may be other filters on `α`
+corresponding to things which are a bit more general than subsets. For example
+if `α` is a topological space and `x : α` then there is a filter `𝓝 x`
+corresponding to "an infinitesimal neighbourhood of `x`" even if
+there is no smallest open set containing `x`. As another example, if `α` has an
+ordering then there is a filter of "neighbourhoods of infinity" on `α` even
+though there might be no `∞` in `α`.
+
+If `F` is a filter, then you can think of `F` as a generalised kind of
+subset `F` of `α`, and you should think of `S ∈ F` as meaning `F ⊆ S`.
+Keeping this in mind will help to motivate the axioms below. 
 
 ## Definition
 
-Here's the definition: a filter on `α` is a collection `F` of subsets
+Here's the formal definition. A filter on `α` is a collection `F` of subsets
 of `α` satisfying the following three axioms:
 
 1) `α ∈ F` (in Lean this is written `univ ∈ F` because a distinction is made
@@ -42,21 +52,31 @@ structure filter (α : Type*) :=
 (inter_sets {x y}       : x ∈ sets → y ∈ sets → x ∩ y ∈ sets)
 ```
 
-A rather simple example of a filter is the filter of all subsets of `α`.
+In words, `filter α` is the type of filters on `α`, so to give a filter
+on `α` is to give a term `F : filter α` of type `filter α`. To make a term
+of type `filter α` you need to give a collection `sets` of subsets of `α`
+and then three proofs of the three axioms.
+
+A rather simple example of a filter is the filter of *all* subsets of `α`.
 Those of you who have seen definitions of filters in other places (for
-example in Bourbaki) might have seen an extra axiom saying that the filter is
-not allowed to be the collection of all subsets of `α`. This turns out to be
-rather an unnatural axiom, it is like demanding in ideal theory that if `R` is a
-ring then `R` is not allowed to be an ideal of `R`. The advantage of such a
-definition of an ideal would be that a maximal ideal of `R` would literally
-be a maximal element of the ideals of `R`, but this advantage is outweighed by
-the disadvantages (e.g. the image of an ideal along a ring homomorphism
-might not be an ideal, you cannot add two ideals etc). To preserve the
-functoriality of filters, mathlib does not have this as an axiom for filters,
-so there are two "extreme" filters on `α`, namely the one which only contains
-`univ` (note that this is forced by `univ_sets`), and the one which contains
-all subsets of `α`. These two filters are called `⊥` and `⊤`, although you might
-be surprised to find out which one is which.
+example in Bourbaki) might have seen an extra axiom in the definition of a
+filter, saying that a filter is not allowed to be the collection of all
+subsets of `α`. This turns out to be rather an unnatural axiom, it is like
+demanding in ideal theory that if `R` is a ring then `R` is not allowed to be
+an ideal of `R`. An advantage of such a definition of an ideal would be that
+a maximal ideal of `R` would literally be a maximal element of the ideals
+of `R`, but this advantage is outweighed by the disadvantage that the
+definition then becomes much less functorial -- e.g. the image of an ideal
+along a ring homomorphism might not be an ideal, you cannot in general add two
+ideals etc). To preserve the functoriality of filters, mathlib does not have
+this Bourbaki axiom as an axiom for filters. As a result there are two
+"extreme" filters on `α`, namely the one which only contains
+`univ` (note that this is forced by `univ_sets`), and then the one we mentioned
+above, which contains all subsets of `α`. These two filters are called
+`⊥` and `⊤`, although you might be surprised to find out which one is which!
+The filter consisting of all subsets of `α` is the one corresponding to
+the empty set, so it is `⊥`, and the one consisting of just `univ` is the
+one corresponding to the whole of `α` so it is `⊤`.
 
 ## Notation, helpful tactics and helpful theorems
 
@@ -71,14 +91,21 @@ the form `∀ S, S ∈ F ↔ S ∈ G`.
 
 The fields of the structure mention things like `S ∈ F.sets`, so the
 axioms are restated with different names, but using the `S ∈ F` notation.
-The lemmas corresponding to the definitions are in the `filter` namespace
-and are called:
+The lemmas corresponding to the definitions are:
 
 `univ_mem_sets : univ ∈ F`
 `mem_sets_of_superset : S ∈ F → S ⊆ T → T ∈ F`
 `inter_mem_sets : S ∈ F → T ∈ F → S ∩ T ∈ F`
 
+These lemmas in the `filter` namespace, i.e. their full names are
+`filter.univ_mem_sets` etc. But we are about to say `open filter`
+which means that you don't have to type this `filter.` thing in front of every
+lemma you need about filters. In fact we'll also be using a bunch of
+stuff about sets, like `set.inter_subset_left`, so why don't we `open set`
+as well.
 -/
+
+open filter set
 
 -- Variables!
 -- let `α` be a type, let `F` be a filter on `α`, and let `S` and `T`
@@ -86,11 +113,10 @@ and are called:
 
 variables (α : Type) (F : filter α) (S T : set α)
 
-open filter set 
-
 /-
-Let's start with a warm-up question: Two sets `S` and `T` are both in
-a filter `F` iff their intersection is. 
+Here's a lemma about filters: Two sets `S` and `T` are both in
+a filter `F` iff their intersection is. See if you can deduce
+it from the axioms of a filter.
 
 For this one it's useful to know the following results (from the set namespace)
 `inter_subset_left S T : S ∩ T ⊆ S`
@@ -118,12 +144,14 @@ Helpful for this exercise:
 `mem_univ s : s ∈ univ`
 `subset.trans : A ⊆ B → B ⊆ C → A ⊆ C`
 `subset_inter : X ⊆ S → X ⊆ T → X ⊆ S ∩ T`
-(but you could probably prove those last two things directly yourself)
+(note that you could probably prove those last two things directly yourself,
+but we may as well use the interface for sets given that it's there)
 `mem_set_of_eq : x ∈ {a : α | p a} = p x`
 (this one is definitional, so you could use `change` instead, or just
 not rewrite it at all)
 -/
 
+-- this is called `𝓟 X` in mathlib but let's just make it ourselves.
 example (X : set α) : filter α :=
 { sets := {S : set α | X ⊆ S},
   univ_sets := begin
@@ -158,10 +186,12 @@ The following is unsurprising: the collection of all filters on `α` is
 partially ordered. Perhaps more surprising: the order is the other way
 around to what you think it should be! If `F` and `G` are filters
 on `α`, then `F ≤ G` is defined to mean that `G.sets ⊆ F.sets`, i.e.
-every set in the `G`-filter is also in the `F`-filter. 
+every set in the `G`-filter is also in the `F`-filter. Why is this?
+Well, think about principal filters. If `S ⊆ T` are two subsets,
+then `X ∈ 𝓟 T` implies `T ⊆ X`, so `S ⊆ X`, so `X ∈ 𝓟 S`. 
+The smaller the set, the bigger the collection of sets in the filter.
 
-Show that this initially crazy-looking definition does play well
-with respect to principal filters, by showing 𝓟 S ≤ 𝓟 T ↔ S ⊆ T.
+Let's formalise this. Show that 𝓟 S ≤ 𝓟 T ↔ S ⊆ T.
 Note that this is called `principal_mono` in mathlib but 
 there's no harm in proving it yourself.
 
@@ -232,11 +262,14 @@ subgroups, and won't talk about it again.
 ### `at_top` filter on a totally ordered set
 
 Let `L` be a non-empty totally ordered set. Let's say that a subset `X` of `L` is
-"big" if there exists `x : L` such for all `y ≥ x`, `y ∈ X`. 
-I claim that the big subsets are a filter. Check this.
+"big" if there exists `x : L` such for all `y ≥ x`, `y ∈ X`.
+
+I claim that the big subsets are a filter. Check this. The mathematical idea
+is that the "big subsets" are the neighbourhoods of `∞`, so the corresponding
+filter is some representation of an infinitesimal neighbourhood of `∞`.
 
 Implementation notes: `linear_order L` is the type of linear orders on `L`.
-`e : L` is just an easy way of saying `L` is nonempty.
+`e : L` is just an easy way of saying that `L` is nonempty.
 
 Recall that `max x y` is the max of x and y in a `linear_order`, and
 `le_max_left a b : a ≤ max a b` and similarly `le_max_right`. 
@@ -284,6 +317,13 @@ Things you might find helpful:
 `finite.subset : S.finite → ∀ {T : set α}, T ⊆ S → T.finite`
 `compl_inter S T : (S ∩ T)ᶜ = Sᶜ ∪ Tᶜ`
 `finite.union : S.finite → T.finite → (S ∪ T).finite`
+
+NB if you are thinking "I could never use Lean by myself, I don't know
+the names of all the lemmas so I have to rely on Kevin telling them all to
+me" then what you don't realise is that I myself don't know the names
+of all the lemmas either -- I am literally just guessing them and pressing
+ctrl-space to check. Look at the names of the lemmas and begin to understand
+that you can probably guess them yourself.
 -/
 
 def cofinite (α : Type) : filter α :=
@@ -310,6 +350,9 @@ def cofinite (α : Type) : filter α :=
 
 ### Exercises (to do on paper):
 
+You don't need to be able to do these in order to move onto the topology part
+of this workshop.
+
 (1) prove that the cofinite filter on a finite type is the entire power set filter.
 (2) prove that the cofinite filter on `ℕ` is equal to the `at_top` filter.
 (3) Prove that the cofinite filter on `ℤ` is not equal to the `at_top` filter.
@@ -317,8 +360,7 @@ def cofinite (α : Type) : filter α :=
 
 You can try them in Lean but you will have to be a master of finiteness.
 Here, for example, are some of the ideas you'll need to do (4) in Lean.
-The proof uses a bunch of lemmas from the set API. You don't need
-to be able to do this to do the topology stuff in the next part. 
+The proof uses a bunch of lemmas from the set API.
 
 Here are some of the things I used:
 
@@ -330,7 +372,8 @@ Facts about `S \ {a}` and other sets:
 `mem_diff_singleton`.
 
 I also needed the following two lemmas, which weren't in mathlib
-so I had to prove them myself:
+so I had to prove them myself (my proof of the first one was longer;
+thanks to Yakov Pechersky on Zulip for coming up with the one-linear)
 -/
 
 lemma infinite_of_finite_compl {α : Type} [infinite α] {s : set α}
@@ -347,7 +390,7 @@ begin
   refl
 end
 
--- Here's a proof which one can formalise: if natural_cofinite = 𝓟 S then S must
+-- Here's a proof which I formalised: if natural_cofinite = 𝓟 S then S must
 -- be cofinite and hence infinite and hence non-empty, but then if a ∈ S
 -- then S \ {a} causes us problems as it's cofinite but doesn't contain `S`.
 theorem cofinite_not_principal : ∀ S : set ℕ, cofinite ℕ ≠ 𝓟 S :=
