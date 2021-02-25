89ec49cd50f4c55536c6a2cb815f9fe0f44e88d5
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-02-25 11:43:25 +0000

    add continuity

--- src/week_6/solutions/Part_B_sequences_again.lean
+++ src/week_6/solutions/Part_B_sequences_again.lean
@@ -20,7 +20,8 @@ definition is_limit (a : ℕ → ℝ) (l : ℝ) : Prop :=
 We then spent some time proving things like
 if aₙ → l and bₙ → m then aₙ * bₙ → l * m.
 
-Let's see another proof of these things using filters.
+Let's see another, much shorter, proof of these things using filters,
+and of course also using facts from `mathlib` about filters.
 
 -/
 
@@ -50,11 +51,17 @@ end
 This was much less painful than what we went through in week 3! So where
 did the work go?
 
-The next 150 lines of this file discuss the first proof, involving
-`metric.tendsto_at_top`. There are no exercises here, so if you
-want to get on with the proving you can skip straight down to
-the `## tendsto.mul` section on line 176 or so, where I talk
-about the second proof and set some exercises.
+The next 120 lines of this file discuss the first proof, namely
+`is_limit_iff_tendsto`. Clearly the key ingredients is
+`metric.tendsto_at_top`. There are no exercises here, I will just
+explain what's going on, and talk about definitions (e.g. `is_limit`)
+and their cost.
+
+The second proof uses `is_limit_iff_tendsto` to reduce `is_limit_mul`
+to a theorem about filters, and them proves it with `tendsto.mul`. We will
+prove our own version of `tendsto.mul` in this file. So if you want to
+get on with the proving you can skip straight down to
+the `## tendsto.mul` section on line 177 or so.
 
 The first proof 
 
@@ -64,7 +71,7 @@ Each *definition* you make in Lean comes with a cost. For example
 check out Lean's definition of `finset`, the type of finite sets.
 Right click on `finset` below and click on "go to definition".
 You see one definition, and then over 2000 lines of theorems
-about this definition. 
+about this definition. Don't forget to close the file afterwards!
 -/
 
 #check finset
@@ -90,7 +97,8 @@ about `is_limit`. This is what happens in an undergraduate analysis
 class -- you see the definition, and then you make what computer
 scientists call the "API" or the "interface" -- a bunch of lemmas
 and theorems about `is_limit`, for example `is_limit_add`, which says
-that `aₙ → l` and `bₙ → m` implies `a_n + b_n → l + m`.
+that `aₙ → l` and `bₙ → m` implies `a_n + b_n → l + m`, and also `is_limit_neg`,
+`is_limit_sub`, `is_limit_mul` and so on.
 
 But it turns out that `is_limit` is just a very special case of `tendsto`,
 and because `tendsto` is already in mathlib, there is already a very big
@@ -101,9 +109,9 @@ lemmas as they used `tendsto` to do other things and then abstracted out
 properties which they discovered were useful. For example, this week
 (I write this in Feb 2021) Heather Macbeth was working on modular forms in Lean
 and she discovered that she needed a lemma about `tendsto`, which, after some
-discussion on the Zulip Lean chat, Heather and I discovered was a statement
-about how `tendsto` commutes with a certain kind of coproduct. Heather is
-right now in the process of adding this lemma (`tendsto.prod_map_coprod`)
+discussion on the Zulip Lean chat, Heather and I realised was a statement
+about how `tendsto` commutes with a certain kind of coproduct. We proved this
+lemma, Heather is right now in the process of adding it (`tendsto.prod_map_coprod`)
 to `mathlib`, Lean's maths library.
 
 https://github.com/leanprover-community/mathlib/pull/6372
@@ -112,8 +120,8 @@ I will remark that I would never have worked on that problem with Heather
 if it hadn't been for the fact that I'd been teaching you about filters
 and hence I had to learn about them properly!
 
-Let's take a look at our proof again. The proof follows from two
-2-line lemmas. I will talk you through the first one, and you can
+Let's take a look at our new proof of `tendsto_mul` again. The proof follows
+from two 2-line lemmas. I will talk you through the first one, and you can
 experiment with the second one. Let's take a look at the first one.
 
 -/
@@ -217,13 +225,13 @@ had to then pay for making it usable was that they had to make a big API for
 `ℝ`, proving stuff like a non-empty bounded set of reals has a least
 upper bound, and that the reals were a topological ring (and hence
 multiplication was continuous). But this price was paid way back in 2018
-so we mathematicians can use these facts for free.
+so we mathematicians can now use these facts for free.
 
 All that remains then, if we want to see the details, is to
 *prove* `tendsto.mul`, and this is a statement about filters on topological
 spaces, so let's do it. First -- what does `continuous` mean?
 
-## continuity
+## Continuity
 
 Let `X` and `Y` be topological spaces, and say `f : X → Y` is a function.
 
@@ -237,9 +245,10 @@ Intuitively, it means that if you move `x` by a small amount, then `f x`
 moves by a small amount. In other words, `f` sends a small neighbourhood
 of `x` into a small neighbourhood of `f x`. 
 
-If our mental model of `𝓝 x` is some kind of generalised set corresponding
-to an infinitesimally small neighbourhood of `x`, you will see why
-Lean makes the following definition of `continuous_at`:
+If our mental model of the neighbourhood filter `𝓝 x` is some kind of
+generalised set corresponding to an infinitesimally small
+neighbourhood of `x`, you will see why Lean makes the following
+definition of `continuous_at`:
 
 -/
 
@@ -253,8 +262,8 @@ end
 /-
 
 Out of interest, you were probably told the definition of what it means
-for a function `f : X → Y` between metric spaces to be continuous at `x`.
-Were you ever told what it means for a function between topological spaces
+for a function `f : X → Y` between *metric* spaces to be continuous at `x`.
+Were you ever told what it means for a function between *topological* spaces
 to be continuous at `x`, rather than just continuous on all of `X`? This
 is what it means.
 
@@ -264,13 +273,11 @@ for the `continuous_at` definition. Don't forget things like
 `tendsto_id : tendsto id x x`
 `tendsto.comp : tendsto g G H → tendsto f F G → tendsto (g ∘ f) F H`
 
-
+from Part A.
 -/
 
-#check tendsto.comp 
-
-
--- this is called `continuous_at_id`. Prove it yourself using `tendsto_id`.
+-- this first lemma called `continuous_at_id`. Prove it yourself using
+-- facts from Part A.
 example (x : X) : continuous_at id x :=
 begin
   exact tendsto_id,
@@ -279,7 +286,8 @@ end
 -- recall we have `f : X → Y`. Now let's add in a `Z`.
 variables (Z : Type) [topological_space Z] (g : Y → Z)
 
--- this is called `continuous_at.comp`. Prove it yourself using `tendsto.comp`.
+-- this is called `continuous_at.comp`. Prove it yourself using
+-- facts from Part A.
 example (x : X) (hf : continuous_at f x) (hg : continuous_at g (f x)) :
 continuous_at (g ∘ f) x :=
 begin
@@ -288,61 +296,102 @@ end
 
 /-
 
-Now we prove a key result, called `continuous_at.prod`.
-It says that if `f : X → Y` and `g : X → Z` are continuous at `x`
-then the product map `f × g : X → Y × Z` is also continuous at `x`.
+Now we prove a key result, called `tendsto.prod_mk_nhds`. Notation for product
+of types: if `Y` and `Z` are types then `Y × Z` is the product type, and 
+the notation for a general term is `(y, z) : Y × Z` with `y : Y` and `z : Z`.
+
+A special case of the theorem below is that if `f : X → Y` and `g : X → Z` are
+continuous at `x` then the product map `f × g : X → Y × Z` is also continuous
+at `x`. We will actually prove something more general -- if `α` is any type
+and `F : filter α` is any filter and if `y : Y` and `z : Z` and if
+`f : α → Y` and `g : α → Z` satisfy `tendsto f F (𝓝 y)` and `tendsto g F (𝓝 z)`,
+then `tendsto (f × g) F (𝓝 (y,z))`, where `f × g` is the map `λ x, (f x, g x)`.
 The key fact you will need from the product topology API is 
 `mem_nhds_prod_iff : S ∈ 𝓝 ((a, b) : X × Y) ↔`
   `∃ (U : set X) (H : U ∈ 𝓝 a) (V : set Y) (H : V ∈ 𝓝 b), U.prod V ⊆ S`
 This is all you should need about the product topology (we won't go into how
-the product topology is defined, but the key fact is that a neighbourhood
-of `(a,b) : X × Y` contains a product of neighbourhoods of `X` and of `Y`).
+the product topology is defined, but the key fact mathematically says that a
+neighbourhood of `(a,b) : X × Y` contains a product of neighbourhoods of `X` and of `Y`).
 
 You will also need to know
 
 `mk_mem_prod : a ∈ U → b ∈ V → (a, b) ∈ U.prod V`
 
 where for `U : set X` and `V : set Y`, `U.prod V = prod U V` is the 
-obvious subset of `X × Y`.
+obvious subset of `X × Y`. 
+
+Recall also from Part A:
+`mem_map : S ∈ map φ F ↔ {x : α | φ x ∈ S} ∈ F`
+`tendsto_def : tendsto f F G ↔ ∀ (S : set Y), S ∈ G → f ⁻¹' S ∈ F`
+(although there is a gotcha here : the actual definition of 
+`tendsto f F G` is `∀ {S : set Y}, S ∈ G ...` )
 -/
 
+#check tendsto_def
+
 -- this is called `tendsto.prod_mk_nhds` in Lean but try proving it yourself.
-example (f : X → Y) (g : X → Z) (x : X) (F : filter X) (y : Y) (z : Z)
+example {α : Type} (f : α → Y) (g : α → Z) (x : X) (F : filter α) (y : Y) (z : Z)
   (hf : tendsto f F (𝓝 y)) (hg : tendsto g F (𝓝 z)) :
   tendsto (λ x, (f x, g x)) F (𝓝 (y, z)) :=
 begin
+  -- say `S` is a neighbourhood of `(y, z)`. 
   rintro S hS,
+  -- use the given fact about neighbourhoods of point in a product of top spaces
   rw mem_nhds_prod_iff at hS,
+  -- to get neighbourhoods of `y` and `z`.
   rcases hS with ⟨U, hU, V, hV, h⟩,
+  -- The goal is `S ∈ map ...` so use `mem_map`.
   rw mem_map,
-  -- I claim {x : X | f x ∈ U} ∈ F
-  have hfxU : {x : X | f x ∈ U} ∈ F := hf hU,
-  -- I claim {x : X | g x ∈ V} ∈ F
-  have hgxV : {x : X | g x ∈ V} ∈ F := hg hV,
-  -- so their intersection is in 𝓝 x 
+  -- I claim {x : α | f x ∈ U} ∈ F
+  have hfxU : {x : α | f x ∈ U} ∈ F := hf hU, -- technical note: I didn't
+                                  -- rewrite `tendsto_def` so I don't supply `U`
+  -- I claim {x : α | g x ∈ V} ∈ F
+  have hgxV : {x : α | g x ∈ V} ∈ F := hg hV,
+  -- so their intersection is in F 
   have hfg := inter_mem_sets hfxU hgxV,
+  -- and so it suffices to show that their intersection contains `{x : α | (f x, g x) ∈ S}`
   refine mem_sets_of_superset hfg _,
+  -- but we just now take everything apart
   rintro x ⟨(hxf : f x ∈ U), (hxg : g x ∈ V)⟩,
   apply h,
+  -- and apply mk_mem_prod
   exact set.mk_mem_prod hxf hxg,
 end
 
--- Armed with `continuous_at.prod`, let's prove `tendsto.mul` in
--- the generality which we need.
-example {X M : Type} [topological_space M] [has_mul M] [has_continuous_mul M]
-  {f g : X → M} {F : filter X} {a b : M} (hf : tendsto f F (𝓝 a))
-  (hg : tendsto g F (𝓝 b)) : tendsto (λ (x : X), f x * g x) F (𝓝 (a * b)) :=
+-- Armed with `tendsto.prod_mk_nhds`, let's prove the version of `tendsto.mul`
+-- which we need
+lemma key_lemma {α M : Type} [topological_space M] [has_mul M] 
+  {f g : α → M} {F : filter α} {a b : M} (hf : tendsto f F (𝓝 a))
+  (hg : tendsto g F (𝓝 b))
+  (hcontinuous : continuous_at (λ (mn : M × M), mn.1 * mn.2) (a,b)) :
+  tendsto (f * g) F (𝓝 (a * b)) :=
 begin
-  set f1 : X → M × M := λ x, (f x, g x) with hf1,
-  set f2 : M × M → M := λ mn, mn.1 * mn.2 with hf2,
-  have h1 : f2 ∘ f1 = f * g,
-  { sorry },
-  intros S hS,
-  rw mem_map,
-
+  set f1 : M × M → M := λ mn, mn.1 * mn.2 with hf1,
+  set f2 : α → M × M := λ x, (f x, g x) with hf2,
+  have h1 : f1 ∘ f2 = f * g,
+  { ext x,
+    refl },
+  have h2 : tendsto f2 F (𝓝 (a, b)) := tendsto.prod_mk_nhds hf hg,
+  rw ← h1,
+  apply tendsto.comp _ h2,
+  apply hcontinuous,
 end
 
+-- The final ingredient is that multiplication is continuous on ℝ, which we
+-- just take from the real API:
+lemma real.continuous_mul_at (a b : ℝ) :
+  continuous_at (λ xy : ℝ × ℝ, xy.1 * xy.2) (a, b) :=
+begin
+  -- it's in the library
+  exact continuous.continuous_at real.continuous_mul,
+end
 
-
-#check continuous_at.prod
-
+-- and now we have all the ingredients we need for our own proof of `is_limit_mul`!
+example  (a b : ℕ → ℝ) (l m : ℝ)
+  (ha : is_limit a l) (hb : is_limit b m) :
+  is_limit (a * b) (l * m) :=
+begin
+  rw is_limit_iff_tendsto at *,
+  apply key_lemma ha hb,
+  apply real.continuous_mul_at,
+end
